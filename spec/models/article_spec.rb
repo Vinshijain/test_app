f7399a6b3b67ed9353a8921993298b489797ca9d
Vinshijain <vinshujain0122@gmail.com>
2017-06-15 18:16:30 +0530

    wrote spec for article model and add validations in the article model.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe Article, type: :model do
     it "should be invalid if title is missing" do
       expect(FactoryGirl.build(:article, title: nil)).to_not be_valid
     end
+    it "should be invalid if description is missing" do
+      expect(FactoryGirl.build(:article, description: nil)).to_not be_valid
+    end
+    it "should be valid if all params are present" do
+      expect(FactoryGirl.build(:article)).to be_valid
+    end
   end
 end
