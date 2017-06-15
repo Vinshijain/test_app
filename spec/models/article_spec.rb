1a089ce6010e8904e3b718d841c87cfc7e91c048
Vinshijain <vinshujain0122@gmail.com>
2017-06-15 18:12:01 +0530

    added a article model.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   describe "Validation" do
     it "should be invalid if title is missing" do
-      FactoryGirl.build(:articel, title: nil).to_not be_valid
+      expect(FactoryGirl.build(:article, title: nil)).to_not be_valid
     end
   end
 end
